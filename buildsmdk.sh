83ba3599c689cc1cc8220a74dec8bd1b52f5a372
sSean Hoyt <seanhoyt963@gmail.com>
2013-10-03 21:18:26 -0500

    Update for new devices and build server

--- buildsmdk.sh
+++ buildsmdk.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-# LiquidSmooth Buildbot v1.0
+# LiquidSmooth Buildbot v1.1
 #
 # By: Shane Faulkner
+# Edited: Sean Hoyt
 # http://shanefaulkner.com
 #
 
@@ -14,32 +15,41 @@ LUNCHCD=()
 #----------------------Default Paths-----------------------#
 
 # your build source code directory path
-SAUCE="/home/liquid/android/liquid"
+SAUCE="/home/teamliquid/2.10"
 
 # your build storage location directory
-STORAGE="/home/liquid/Desktop/builds"
+STORAGE="/home/www/devs/teamliquid/Nightlys"
 
 #--------------------Default Settings----------------------#
 
 # your package name and version number
-VERSION="Liquid-JB-v2.8-OFFICIAL"
+VERSION="Liquid-JB-v2.10-Nightly"
 
 #--------------------Default Products----------------------#
 
 PRODUCT[1]="i9300"
 LUNCHCD[1]="liquid_i9300-userdebug"
 
-PRODUCT[2]="i605"
-LUNCHCD[2]="liquid_i605-userdebug"
+PRODUCT[2]="i9305"
+LUNCHCD[2]="liquid_i9305-userdebug"
 
-PRODUCT[3]="l900"
-LUNCHCD[3]="liquid_l900-userdebug"
+PRODUCT[3]="i605"
+LUNCHCD[3]="liquid_i605-userdebug"
 
-PRODUCT[4]="n7100"
-LUNCHCD[4]="liquid_n7100-userdebug"
+PRODUCT[4]="l900"
+LUNCHCD[4]="liquid_l900-userdebug"
 
-PRODUCT[5]="t0lte"
-LUNCHCD[5]="liquid_t0lte-userdebug"
+PRODUCT[5]="n7100"
+LUNCHCD[5]="liquid_n7100-userdebug"
+
+PRODUCT[6]="t0lte"
+LUNCHCD[6]="liquid_t0lte-userdebug"
+
+PRODUCT[7]="t0lteatt"
+LUNCHCD[7]="liquid_t0lteatt-userdebug"
+
+PRODUCT[8]="t0ltetmo"
+LUNCHCD[9]="liquid_t0ltetmo-userdebug"
 
 #---------------------Buildbot Code------------------------#
 
